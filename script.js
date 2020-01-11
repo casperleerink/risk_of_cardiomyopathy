73481f717180d67d2d39e2268886783adf7ad9b1
casperleerink <casperleerink@gmail.com>
2020-01-11 10:50:59 -0800

    first commit function not workable yet

---
+++ script.js
@@ -0,0 +1,31 @@
+function updateTextInput(val) {
+    var result = (val * 100).toFixed(0) + "%";
+    document.getElementById('rangeValue').value=result; 
+}
+
+function calcRisk(ef, ant, chestrt) {
+    var c1 = -0.227136985;
+    var c2 = 0.003540834;
+    var c3 = 0.055104531;
+    var c4 = -11.71785;
+    var c5 = 0.00901034;
+    var interim = -1.0 * (Math.exp(((ef*c1)+(ant * c2)+(chestrt * c3)) - c4) * c5);
+    console.log(interim);
+    
+    console.log(Math.exp(interim));
+    
+    var result = (1.0 - Math.exp( -1.0*( Math.exp(((ef*c1)+(ant * c2)+(chestrt * c3)) - c4) *  (c5) )));
+    console.log(result);
+    
+    return result;
+}
+
+function getResult() {
+    var ef = parseFloat(document.getElementById('EF').value);
+    var ant = parseFloat(document.getElementById('ANT').value);
+    var chestrt = parseFloat(document.getElementById('CHESTRT').value);
+    var result = calcRisk(ef, ant, chestrt);
+    var percent = (result * 100).toFixed(0) + "%";
+    var resultDiv = document.getElementById('results');
+    resultDiv.innerHTML = "<h2>Result</h2><p>You have a " + percent + " risk of getting cardiomyopathy</p>";
+}
