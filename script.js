1dd04b6ccbd090dd0942cd64c5aba67e5a686310
janleerink <59804857+janleerink@users.noreply.github.com>
2020-03-09 09:49:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 
 function calcRisk(ef, ant, chestrt) {
-    var c1 = -0.227136985;
-    var c2 = 0.003540834;
-    var c3 = 0.055104531;
-    var c4 = -11.71785;
-    var c5 = 0.00901034;
+    var c1 = -0.226519184;
+    var c2 = 0.003633632;
+    var c3 = 0.051286179;
+    var c4 = -11.67149;
+    var c5 = 0.009061703;
     var result = (1.0 - Math.exp( -1.0*( Math.exp(((ef*c1)+(ant * c2)+(chestrt * c3)) - c4) *  (c5) )));
     
     return result;
